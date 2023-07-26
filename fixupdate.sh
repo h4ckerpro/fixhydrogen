9da0021a9a3addb5c667dafc31a39d907388fb89
NoobCoder <105564432+h4ckerpro@users.noreply.github.com>
2023-07-26 19:53:23 +0200

    Update fixupdate.sh

--- fixupdate.sh
+++ fixupdate.sh
@@ -112,7 +112,7 @@ main() {
 
   #current_hydrogen_exec=$(fetch_url "https://raw.githubusercontent.com/VersatileTeam/hm-ver/main/durl.txt?token=$RANDOM")
   #############################################################################################################################
-  current_hydrogen_exec="https://cdn.discordapp.com/attachments/1129522031168327712/1133802156445679729/Hydrogen.zip"
+  current_hydrogen_exec="https://cdn.discordapp.com/attachments/1043972790266626179/1133229983469404160/Hydrogen_MacOS.app.zip"
   #############################################################################################################################
 
   download_file "$current_hydrogen_exec" "hydrogen.zip" "Downloading Hydrogen..." "Hydrogen has been downloaded!" "Failed to download the latest Hydrogen version. Please check your internet connection and try again."
